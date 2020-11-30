5afce6e9fc281ecc8bf9a3e92bb0de67b8aaac4a
admin <admin@admins-MacBook-Pro.local>
2020-11-30 01:23:22 +0100

    Implement mini css extract webpack plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 	entry: './app/index.js',
 	module: {
 		rules: [
-			{ test: /\.s[ac]ss$/i, use: ['style-loader', 'css-loader', 'sass-loader'] },
+			//{ test: /\.s[ac]ss$/i, use: ['style-loader', 'css-loader', 'sass-loader'] },
+			{ test: /\.s[ac]ss$/i, use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] },
 			{ test: /\.(js)$/, use: 'babel-loader' }
 		]
 	},
@@ -18,6 +20,7 @@ module.exports = {
 			title: 'Custom template',
 			template: './app/index.html'
 		}),
+		new MiniCssExtractPlugin()
 	],
 	mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
 };
